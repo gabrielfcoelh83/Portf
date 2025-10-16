ce6136cb2bc7ac4918a895fe27009c1e52ec2ca0
gabrielfcoelh83 <gabrielfcolh83@gmail.com>
2025-10-16 16:45:56 -0300

    fix: update project title to Portuguese and correct typo in AnimationComponent

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -49,12 +49,12 @@ const Projects: React.FC = () => {
         <StyledExperience>
             <Container maxWidth="lg">
                 <Box id="projects" pt={5} pb={3}>
-                    <Typography variant="h2" textAlign="center" color="primary.contrastText">Projects</Typography>
+                    <Typography variant="h2" textAlign="center" color="primary.contrastText">Projetos</Typography>
                 </Box>
                 <Grid container spacing={5} pb={3}>
                     {projects.map((project: ProjectCardProps, index: number) => (
                         <Grid size={{ md: 6 }} key={index}>
-                            <AnimationComponent moveDirection={index % 2 == 0 ? "right" : "left"}>
+                            <AnimationComponent moveDirection={index % 2 == 0 ? "right" : "left"}>s
                                 <ProjectCard
                                     title={project.title}
                                     subtitle={project.subtitle}
